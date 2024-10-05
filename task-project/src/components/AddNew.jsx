c055a01b0520c284e8ebc2fc8298c4d7c52b2bc3
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-10-05 15:43:21 +0530

    Project Completed

--- task-project/src/components/AddNew.jsx
+++ task-project/src/components/AddNew.jsx
@@ -4,7 +4,7 @@ const AddNew = ({ setOpen }) => {
     <>
       <button
         className="shadow-lg bg-black text-white hover:bg-white hover:text-black text-md px-5 py-2 rounded-md italic hover:border hover:border-solid hover:border-black"
-        onClick={setOpen(true)}
+        onClick={() => setOpen(true)}
       >
         Add New
       </button>
